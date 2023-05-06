d3c12d19f3c8c129e67aefd8798054cd59f66950
TetianaUMKA <113261486+TetianaUMKA@users.noreply.github.com>
2023-05-06 23:40:25 +0300

    Create Section.styled.jsx

---
+++ src/components/Section/Section.styled.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const SiteSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  padding-top: 40px;
+`;

a137676a86375bbba0801a518eeff5fe24156956
TetianaUMKA <113261486+TetianaUMKA@users.noreply.github.com>
2023-05-06 23:40:37 +0300

    Create FeedbackOptions.styled.jsx

---
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const FeedbackItem = styled.li`
+  :not(:last-child) {
+    margin-bottom: 10px;
+  }
+`;
+
+export const FeedbackBtn = styled.button`
+  width: 100px;
+  padding: 10px;
+  border-radius: 4px;
+  border: transparent;
+  background-image: linear-gradient(to top, #4481eb 0%, #04befe 100%);
+
+  :hover {
+    box-shadow: 1px 5px 4px 1px rgba(7, 20, 56, 0.53);
+    transition: box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  }
+`;

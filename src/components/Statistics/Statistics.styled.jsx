ac9029b6fffce5e24104747195f577f40d5e1962
TetianaUMKA <113261486+TetianaUMKA@users.noreply.github.com>
2023-05-06 23:40:50 +0300

    Create Statistics.styled.jsx

---
+++ src/components/Statistics/Statistics.styled.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const StatItem = styled.li`
+  :nth-child(-n + 2) {
+    margin-bottom: 8px;
+  }
+
+  :nth-child(3) {
+    margin-bottom: 14px;
+  }
+
+  :nth-child(4) {
+    margin-bottom: 20px;
+  }
+`;
+
+export const StatItemData = styled.p`
+  display: flex;
+  align-items: flex-end;
+`;

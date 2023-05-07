cbdc635a79b3d8b624e8ea645e24bc256cc1e160
TetianaUMKA <113261486+TetianaUMKA@users.noreply.github.com>
2023-05-07 21:52:10 +0300

    Update FeedbackOptions.styled.jsx

--- src/components/FeedbackOptions/FeedbackOptions.styled.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -1,9 +1,8 @@
 import styled from 'styled-components';
 
-export const FeedbackItem = styled.li`
-  :not(:last-child) {
-    margin-bottom: 10px;
-  }
+export const FeedbackList = styled.ul`
+  display: flex;
+  gap: 10px;
 `;
 
 export const FeedbackBtn = styled.button`
